19648e305f07b0718808a1a2ea589fa51e6b6fa5
iamcpm <chhaya@miminawewe.com>
2025-08-24 15:54:32 +0000

    change code to reduce ball size on click

--- script.js
+++ script.js
@@ -23,10 +23,32 @@ function preload() {
     this.load.image("ball", "assets/ball.png"); // watch out for case sensitivity
 }
 
-function create() {
+/*function create() {
     ball = this.add.sprite(WIDTH / 2, HEIGHT / 2, "ball"); // x, y, and the ball "key"
-    ball.setDisplaySize(ballSize, ballSize); // width, height
-}
+    ball.setDisplaySize(ballSize, ballSize); // width, height*/
+
+        // ...existing code...
+    
+        function create() {
+        ball = this.add.sprite(WIDTH / 2, HEIGHT / 2, "ball");
+        ball.setDisplaySize(ballSize, ballSize);
+
+        // Make the ball interactive
+        ball.setInteractive();
+
+        // On pointerdown (click), shrink and speed up
+        ball.on('pointerdown', () => {
+            // Reduce size by 10%
+            ballSize *= 0.9;
+            ball.setDisplaySize(ballSize, ballSize);
+
+            // Speed up by 10%
+            xspeed *= 1.1;
+            yspeed *= 1.1;
+        });
+    }
+    // ...existing code...
+
 
 function update() {
     ball.y += yspeed;
